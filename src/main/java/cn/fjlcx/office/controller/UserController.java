2b345f2df5c44f5b15aada70cde16411fc52e18f
smlcx <@sqf519205>
2017-06-27 17:27:20 +0800

    init

--- src/main/java/cn/fjlcx/office/controller/UserController.java
+++ src/main/java/cn/fjlcx/office/controller/UserController.java
@@ -1,14 +1,20 @@
 package cn.fjlcx.office.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Created by lcx on 2017/6/27.
  */
-@Controller
+@RestController
 class UserController {
-	@RequestMapping("user")
+	@RequestMapping(value = "user",method = RequestMethod.GET)
+	@ApiOperation(value = "获取用户列表", notes = "")
+	@ResponseBody
 	public void user(){
 		System.out.print("hello world");
 	}
